a6938de3c15cdd0e78b623b5e56419a255c8990e
方汉佳 <1690101402@qq.com>
2018-04-28 20:11:46 +0800

    添加了后端-apiResult文件

---
+++ src/server/api/apiResult.js
@@ -0,0 +1,4 @@
+// 用于操作返回信息的模块
+module.exports = function(status,data,message){
+    return {status,data,message};
+}
---
+++ src/server/api/db.js
@@ -0,0 +1,3 @@
+// 用于操作数据库的模块
+const apiResult = require('./apiResult');
+const mc = require('');
