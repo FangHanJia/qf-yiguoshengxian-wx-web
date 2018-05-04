a0ed4773d3c516d7bc8797b4bb0ebcd1880ab718
T-0071 <2093785237@qq.com>
2018-05-04 22:21:16 +0800

    修改

--- src/components/index/header/header.vue
+++ src/components/index/header/header.vue
@@ -3,6 +3,7 @@
     <diV id="nav" class="ml">
       <p>广州<i class="iconfont icon-icon--1"style="color:#fff;"></i></p>
       <input type="text" />
+      
       <span></span>
     </div>
 </template>
